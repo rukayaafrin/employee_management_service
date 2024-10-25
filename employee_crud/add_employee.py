0d7dd3c43a38629bfe0170364bf863a86a1ed405
rukayaafrin <e0639627@u.nus.edu>
2024-10-25 22:08:13 +0800

    Update to use create_dict

--- employee_crud/add_employee.py
+++ employee_crud/add_employee.py
@@ -1,19 +1,29 @@
 from utils.ansi_colors import text_colours
+from utils.dict_helper import create_dict  
 
 def add_employee(employees):
     print(text_colours['CYAN'] + "Add Employee" + " ✍️" + text_colours['RESET'] )
     try:
-        employee = {
-            "id": input("Enter employee id: "),
-            "name": input("Enter employee name: "),
-            "age": int(input("Enter employee age: ")),
-            "department": input("Enter employee department: "),
-            "city": input("Enter employee city: "),
-            "salary": float(input("Enter employee salary: "))  
-        }
+       
+        address = create_dict(
+            street=input("Enter employee street: "),
+            city=input("Enter employee city: "),
+            postalcode=input("Enter employee postal code: ")
+        )
+        
+        
+        employee = create_dict(
+            id=input("Enter employee id: "),
+            name=input("Enter employee name: "),
+            age=int(input("Enter employee age: ")),
+            department=input("Enter employee department: "),
+            address=address,  
+            salary=float(input("Enter employee salary: "))
+        )
+        
         if employee["id"] in [employee["id"] for employee in employees]:
             print(text_colours['RED'] + "Employee id already exists" + " ❌" + text_colours['RESET'])
-        elif any(value == "" for value in employee.values()):
+        elif any(value == "" for value in employee.values() if not isinstance(value, dict)) or any(value == "" for value in address.values()):
             print(text_colours['RED'] + "All fields are required. Please fill in all the details." + " ❌" + text_colours['RESET'])
         else:
             employees.append(employee)
